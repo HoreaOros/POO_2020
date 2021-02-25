53e58507978aa7e310de5c327cf853039d7cee31
Timi <timi.hasas@gmail.com>
2021-02-25 12:24:45 +0200

    added ladder

--- Bars/Ladder.cs
+++ Bars/Ladder.cs
@@ -16,11 +16,13 @@
     /// </summary>
     internal class Ladder
     {
-        
-
+        Frame f1, f2;
+        VertBar v;
         public Ladder(int width, int height)
         {
-
+            f1 = new Frame(width, height);
+            v = new VertBar(height);
+            f2 = new Frame(width, height);
         }
     }
 }
