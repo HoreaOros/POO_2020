f039410472f5953c7f3369448f8cc2e106dcb516
Timi Hasas <timi.hasas@gmail.com>
2021-02-25 22:43:02 +0200

    TODO Parsing

--- Complex/Complex.cs
+++ Complex/Complex.cs
@@ -7,17 +7,85 @@ namespace ComplexNumbers
         private double re;
         private double im;
 
-        public Complex(double re): this(re, 0)
+        public Complex(double re) : this(re, 0)
         {
 
         }
 
 
-        // TODO: parsing pe string pentru a scoate partea reala si partea imaginara
+        // TODO: parsing pe string pentru a scoate partea reala si partea imaginara (kinda done)
         // probabil ca s-ar putea face cu expresii regulare. 
         public Complex(string v)
         {
-            
+            double re, im;
+            int i, k = 1, nr = 0;
+            double zec = 0;
+            int len = v.Length;
+            bool isInt = true;
+            int sign = 1;
+            for (i = 0; i < len; i++)
+            {
+                if (v[i] <= '9' && v[i] >= '0' && isInt)
+                {
+                    nr = nr * k + (int)char.GetNumericValue(v[i]);
+                    k *= 10;
+                }
+                else
+                if (v[i] == '.')
+                {
+                    isInt = false;
+                    k = 10;
+                }
+                else
+                if (v[i] <= '9' && v[i] >= '0' && !isInt)
+                {
+                    zec = (zec * k + (int)char.GetNumericValue(v[i])) / k;
+                    k *= 10;
+                }
+                else
+                {
+                    if (v[i] == '+')
+                    {
+                        sign = 1;
+                        break;
+                    }
+                    else
+                        if (v[i] == '-')
+                    {
+                        sign = -1;
+                        break;
+                    }
+                }
+            }
+
+            isInt = true;
+            re = nr + zec;
+            nr = 0; zec = 0; k = 1;
+
+            for (int j = i; j < len; j++)
+            {
+                if (v[j] <= '9' && v[j] >= '0' && isInt)
+                {
+                    nr = nr * k + (int)char.GetNumericValue(v[j]);
+                    k *= 10;
+                }
+                else
+                if (v[j] == '.')
+                {
+                    isInt = false;
+                    k = 10;
+                }
+                else
+                if (v[j] <= '9' && v[j] >= '0' && !isInt)
+                {
+                    zec = (zec * k + (int)char.GetNumericValue(v[j])) / k;
+                    k *= 10;
+                }
+            }
+
+            im = sign * (nr + zec);
+            this.re = re;
+            this.im = im;
         }
 
         public Complex(double re, double im)
@@ -29,13 +97,13 @@ namespace ComplexNumbers
         // TODO: modificat codul in asa fel incat sa tina cont de valori 0 sau negative.(done)
         public override string ToString()
         {
-            if(re == 0)
+            if (re == 0)
                 return "(" + im.ToString() + "i" + ")";
 
-            if(im == 0)
+            if (im == 0)
                 return "(" + re.ToString() + ")";
 
-            if(im < 0)
+            if (im < 0)
                 return "(" + re.ToString() + im.ToString() + "i" + ")";
 
             return "(" + re.ToString() + "+" + im.ToString() + "i" + ")";
@@ -72,8 +140,8 @@ namespace ComplexNumbers
                 return re;
             }
         }
-        public double Imag 
-        { 
+        public double Imag
+        {
             get
             {
                 return im;
