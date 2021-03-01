9b5461029abd9e5e249e53bcef026b1f5e73512e
samivagyok-web <samivagyok@gmail.com>
2021-03-01 09:12:52 +0200

    .

--- Complex/Complex.cs
+++ Complex/Complex.cs
@@ -18,17 +18,21 @@ namespace ComplexNumbers
         // probabil ca s-ar putea face cu expresii regulare. 
         public Complex(string v)
         {
-            Regex.Replace(v, @"\s+", "");
+            v = Regex.Replace(v, @"\s+", "");
 
-            Regex realPattern = new Regex(@"^(-|\+|)\d*");
+            Regex realPattern = new Regex(@"^(-|\+|)\d*(?!i)");
             if (realPattern.IsMatch(v))
             {
                 MatchCollection realMatches = realPattern.Matches(v);
-                this.re = double.Parse(realMatches[0].ToString());
+
+                if (realMatches[0].ToString().Length != 0)
+                {
+                    this.re = double.Parse(realMatches[0].ToString());
+                }
             }
 
-            Regex imagPattern = new Regex(@"(\+|-)\d*(?=\s*i)");
-            if (realPattern.IsMatch(v))
+            Regex imagPattern = new Regex(@"(\+|-|)\d*(?=i$)");
+            if (imagPattern.IsMatch(v))
             {
                 MatchCollection imagMatches = imagPattern.Matches(v);
                 this.im = double.Parse(imagMatches[0].ToString());
