6863d5c4ed45c56196bc20c2217ec39c088d976a
Horea Oros <horea.oros@gmail.com>
2021-04-15 14:28:19 +0300

    fix builder test

--- DesignPatterns/DesignPatternsTests/Builder/BuilderTests.cs
+++ DesignPatterns/DesignPatternsTests/Builder/BuilderTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesignPatterns.Builder.LunchOrder.Tests
+namespace DesignPatterns.Builder.Tests
 {
     [TestClass()]
     public class BuilderTests
@@ -15,12 +15,21 @@ namespace DesignPatterns.Builder.LunchOrder.Tests
         public void GetLunchOrderTest()
         {
             // arrange
-            
+            LunchOrder.Builder builder = new LunchOrder.Builder();
 
             // act
-
+            LunchOrder order =
+                builder.Bread("Wheat")
+                .Dressing("Mayo")
+                .Meat("Turkey")
+                .GetLunchOrder();
 
             // assert
+            Assert.AreEqual(order.Bread, "Wheat");
+            Assert.AreEqual(order.Condiments, null);
+            Assert.AreEqual(order.Dressing, "Mayo");
+            Assert.AreEqual(order.Meat, "Turkey");
+
         }
     }
 }
